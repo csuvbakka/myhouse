eb691ea597cc5cdb91e0d471450144dae56ee428
csuvbakka <csuvbakka@gmail.com>
2018-10-14 18:09:11 +0200

    use Flask

---
+++ web/index.py
@@ -0,0 +1,40 @@
+from flask import Flask, session, redirect, url_for, render_template, request
+
+
+app = Flask(__name__)
+with open('key.txt') as f:
+    key = f.readline().rstrip()
+app.secret_key = key
+
+
+def is_valid_login(password):
+    with open('pasword.txt') as f:
+        valid_password = f.readline().rstrip()
+    return password == valid_password
+
+
+@app.route('/')
+def index():
+    if 'logged_in' in session and session['logged_in'] == True:
+        return '<a href="/logout">Logout</a>'
+
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST' and is_valid_login(request.form['password']):
+        session['logged_in'] = True
+        return redirect(url_for('index'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return redirect(url_for('login'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
